60324c908580eba1332e66ab2f6a23f3a7ea24bc
resting <roy.wong.80@gmail.com>
2017-02-25 22:17:13 +0800

    Step 2: Create react components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Input from './components/Input';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>Simple React Redux example</h1>
+        <div class="container">
+          <div class="top">
+            <Input />
+          </div>
+          <div className="flex">
+            <div className="fcol">
+              <h3>Store's state:</h3>
+              <span className="state-value"></span>
+            </div>
+            <div className="fcol">
+              <h3>Character count:</h3>
+              <span className="char-count"></span>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ src/components/Input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Input extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <textarea></textarea><br />
+        <button>Count</button>
+      </div>
+    );
+  }
+
+}
+
+export default Input;
