7a73805998f20c6cd0caae4bbf7fd78e3ea7a84d
resting <roy.wong.80@gmail.com>
2017-02-26 02:45:54 +0800

    Step 6: Pass Action Creator to Input as props

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
 
 class Input extends React.Component {
 
+  handleClick() {
+    const { countChar } = this.props;
+    countChar(this.refs.textarea.value);
+  }
+
   render() {
+
     return (
       <div>
-        <textarea></textarea><br />
-        <button>Count</button>
+        <textarea ref="textarea"></textarea><br />
+        <button onClick={this.handleClick.bind(this)}>Count</button>
       </div>
     );
   }
-
 }
 
-export default Input;
+export default connect(null, actions)(Input);
