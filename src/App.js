3581d944729664c5a9b3a4410f13bdba6f6726dc
resting <roy.wong.80@gmail.com>
2017-02-26 03:08:39 +0800

    Step 8: Create Provider, Store and render the 2 extracted components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import Input from './components/Input';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import appReducers from './reducers';
+import ShowState from './containers/showState';
+import ShowCount from './containers/showCount';
 
 class App extends Component {
   render() {
+    const store = createStore(appReducers);
     return (
-      <div className="App">
-        <h1>Simple React Redux example</h1>
-        <div class="container">
-          <div class="top">
-            <Input />
-          </div>
-          <div className="flex">
-            <div className="fcol">
-              <h3>Store's state:</h3>
-              <span className="state-value"></span>
+      <Provider store={store}>
+        <div className="App">
+          <h1>Simple React Redux example</h1>
+          <div className="container">
+            <div className="top">
+              <Input />
             </div>
-            <div className="fcol">
-              <h3>Character count:</h3>
-              <span className="char-count"></span>
+            <div className="flex">
+              <ShowState />
+
+              <ShowCount />
             </div>
           </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
